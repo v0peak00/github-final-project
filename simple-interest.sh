538c210dd6ee99dd096462509cc7b8250c727e4d
Aki Peltokangas <90895557+v0peak00@users.noreply.github.com>
2023-07-05 18:36:50 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # v0peak00
    # Input:
    # p, principal amount
    # t, time period in years
